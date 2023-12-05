d45e906aae5103610a4a5b873064ced609d4f4d9
Rahul Ravi Mahatha <135226419+RahulMht@users.noreply.github.com>
2023-12-05 10:22:16 +0000

    Adding Computer vision project, FM Capstone project: Optical Mark Recognition (OMR)

---
+++ OMR project/utils.py
@@ -0,0 +1,113 @@
+import cv2
+import numpy as np
+
+## TO STACK ALL THE IMAGES IN ONE WINDOW
+def stackImages(imgArray,scale,lables=[]):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        hor_con= np.concatenate(imgArray)
+        ver = hor
+    if len(lables) != 0:
+        eachImgWidth= int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        #print(eachImgHeight)
+        for d in range(0, rows):
+            for c in range (0,cols):
+                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
+                cv2.putText(ver,lables[d][c],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
+    return ver
+
+def reorder(myPoints):
+
+    myPoints = myPoints.reshape((4, 2)) # REMOVE EXTRA BRACKET
+    myPointsNew = np.zeros((4, 1, 2), np.int32) # NEW MATRIX WITH ARRANGED POINTS
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)]  #[0,0]
+    myPointsNew[3] =myPoints[np.argmax(add)]   #[w,h]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] =myPoints[np.argmin(diff)]  #[w,0]
+    myPointsNew[2] = myPoints[np.argmax(diff)] #[h,0]
+
+    return myPointsNew
+
+def rectContour(contours):
+
+    rectCon = []
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 1900 and area < 130000:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if len(approx) == 4:
+                rectCon.append(i)
+    rectCon = sorted(rectCon, key=cv2.contourArea,reverse=True)
+    #print(len(rectCon))
+    return rectCon
+
+def getCornerPoints(cont):
+    peri = cv2.arcLength(cont, True) # LENGTH OF CONTOUR
+    approx = cv2.approxPolyDP(cont, 0.02 * peri, True) # APPROXIMATE THE POLY TO GET CORNER POINTS
+    return approx
+
+def splitBoxes(img1,img2):
+    img1 = cv2.resize(img1, (500, 700))
+    img2 = cv2.resize(img2, (500, 700))    
+    img = np.concatenate((img1, img2), axis=0)
+    
+
+    rows = np.vsplit(img,50)
+    boxes=[]
+    for r in rows:
+        cols= np.hsplit(r,5)
+        for box in cols[1:]:
+            boxes.append(box)
+    return boxes
+
+def showAnswers(img,myIndex,grading,ans,questions=25,choices=4):
+     secW = int(img.shape[1]/(choices+1))+5
+     secH = int(img.shape[0]/(questions))
+    
+
+     for x in range(0,questions):
+         myAns= myIndex[x]
+         cX = ((myAns+1) * secW) + secW // 2
+         cY = (x * secH) + secH // 2
+         if grading[x]==1:
+            myColor = (255,0,0)
+            #cv2.rectangle(img,(myAns*secW,x*secH),((myAns*secW)+secW,(x*secH)+secH),myColor,cv2.FILLED)
+            cv2.circle(img,(cX,cY),12,myColor,cv2.FILLED)
+         else:
+            myColor = (60,0,255)
+            #cv2.rectangle(img, (myAns * secW, x * secH), ((myAns * secW) + secW, (x * secH) + secH), myColor, cv2.FILLED)
+            cv2.circle(img, (cX, cY), 12, myColor, cv2.FILLED)
+
+            # CORRECT ANSWER
+            myColor = (0, 255, 0)
+            correctAns = ans[x]
+            cv2.circle(img,(((correctAns+1) * secW)+secW//2, (x * secH)+secH//2),
+            10,myColor,cv2.FILLED)
+
+
+
