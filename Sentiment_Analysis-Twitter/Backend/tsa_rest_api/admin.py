ce32a4592c12853cd187ee7d596e886df2d81dd8
Rahul Ravi Mahatha <135226419+RahulMht@users.noreply.github.com>
2023-12-14 11:46:22 +0000

    deleted previous project

--- Sentiment_Analysis-Twitter/Backend/tsa_rest_api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from tsa_rest_api.models import ClassifiedTweets
-
-# Register your models here.
-
-admin.site.register(ClassifiedTweets)
--- Sentiment_Analysis-Twitter/Backend/tsa_rest_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-from tsa_rest_api import views
-
-urlpatterns = [
-    re_path('classified-tweets', views.index),
-    re_path('classified-tweets/<int:id>', views.index),
-    re_path('train-classifier', views.train_classifier),
-    re_path('fetch-tweets', views.classify_tweets),
-]
